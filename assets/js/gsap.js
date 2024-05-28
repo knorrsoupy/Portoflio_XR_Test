8b4c1353a6c958dfc69bbd80472e541be5bdb990
Souparna Roy <souparnarroy@gmail.com>
2024-05-29 00:26:14 +0530

    gsap added to the project --small implementation

---
+++ assets/js/gsap.js
@@ -0,0 +1,58 @@
+
+ // use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger,EaselPlugin);
+  // gsap code here!
+    gsap.from('.hero-content', {
+        duration: 1,
+        y: -100,
+        opacity: 0,
+        ease: 'power4.out',
+    });
+
+    gsap.from('.w-100', {
+        duration: 1,
+        y: 100,
+        opacity: 0,
+        ease: 'power4.out',
+    });
+
+   gsap.from('.card', {
+        duration: 1,
+        y: 100,
+        opacity: 0,
+        ease: 'power4.out',
+        scrollTrigger: {
+            trigger: '.card',
+            start: 'top 80%',
+        }
+   });
+    
+   gsap.from('.portfolio-card', {
+    duration: 1.5,
+        y: 100,
+        opacity: 0,
+        ease: 'power4.out',
+        scrollTrigger: {
+            trigger: '.card',
+            start: 'top 80%',
+        }
+   });
+
+   gsap.from('.project-banner', {
+    duration: 1.5,
+        y: 100,
+        opacity: 0,
+        ease: 'power4.out',
+        scrollTrigger: {
+            trigger: '.card',
+            start: 'top 80%',
+        }
+   });
+
+   
+
+});
+
+
+
