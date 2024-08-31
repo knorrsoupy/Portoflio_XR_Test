a242a1570980c28cb87ee6d3472f7d1ee96ea75a
Souparna Roy <souparnarroy@gmail.com>
2024-08-31 16:39:14 +0530

    check

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@
  */
 
 const addEventOnElements = function (elements, eventType, callback) {
-  for (let i = 0, len = elements.length; i < len; i++) {
+  for (let i = 0, len = elements.length/2; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
   }
 }
